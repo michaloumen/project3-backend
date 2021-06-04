fbfbdf59d0bfa102c0dbaef52bcdead99de7c920
michaloumen <michelle.lmendonca@gmail.com>
2021-06-04 20:18:09 -0300

    adicionando quantidade em estoque

--- data.js
+++ data.js
@@ -6,7 +6,8 @@ export default {
             category: "Clínica Geral",
             image: "/images/jogoClinico.jpg",
             price: "80,00",
-            brand: "Golgran"
+            brand: "Golgran",
+            countInStock: 6
         },
         {
             _id: '2',
@@ -14,7 +15,8 @@ export default {
             category: "Biossegurança",
             image: "/images/fitaAutoclave.jpg",
             price: "7,00",
-            brand: "Cremer"
+            brand: "Cremer",
+            countInStock: 20
         },
         {
             _id: '3',
@@ -22,7 +24,8 @@ export default {
             category: "Endodontia",
             image: "/images/limaKPrimeiraSérie.jpg",
             price: "25,90",
-            brand: "Golgran"
+            brand: "Golgran",
+            countInStock: 6
         },
         {
             _id: '4',
@@ -30,7 +33,8 @@ export default {
             category: "Descartáveis",
             image: "/images/sugadorEndodonticoDescartavel.jpg",
             price: "14,90",
-            brand: "SSPlus"
+            brand: "SSPlus",
+            countInStock: 0
         },
         {
             _id: '5',
@@ -38,7 +42,8 @@ export default {
             category: "Periodontia",
             image: "/images/kitPerio.jpg",
             price: "229,09",
-            brand: "Millennium - Golgran"
+            brand: "Millennium - Golgran",
+            countInStock: 10
         },
         {
             _id: '6',
@@ -46,7 +51,8 @@ export default {
             category: "Endodontia",
             image: "/images/waveOneGold.jpg",
             price: "284,31",
-            brand: "Maillefer - Densply Sirona"
+            brand: "Maillefer - Densply Sirona",
+            countInStock: 11
         },
     ]
 }
--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:3000', //habilitando somente o front a consumir nossa API
+    origin: 'http://localhost:3001', //habilitando somente o front a consumir nossa API
 }));//habilitando nossa API para receber requisições de qualquer origem
 
 app.get("/api/products", (req, res) => {
     res.send(data.products);
 });
 
-app.listen(5000, () => {console.log("Server started at http://localhost:5000")})
+app.listen(5000, () => { console.log("Server started at http://localhost:5000") })
