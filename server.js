9dc20775ff71e625089f5bf63341677ec42b6147
michaloumen <michelle.lmendonca@gmail.com>
2021-06-08 22:45:06 -0300

    usando redux na pagina de produtos

--- server.js
+++ server.js
@@ -8,6 +8,15 @@ app.use(cors({
     origin: 'http://localhost:3001', //habilitando somente o front a consumir nossa API
 }));//habilitando nossa API para receber requisições de qualquer origem
 
+app.get("/api/products/:id", (req, res) => {
+    const productId = req.params.id;
+    const product = data.products.find(x => x._id === productId);
+    if (product)
+        res.send(product);
+    else
+        res.status(404).send({ msg: "Product Not Found." })
+});
+
 app.get("/api/products", (req, res) => {
     res.json(data.products);
 });
