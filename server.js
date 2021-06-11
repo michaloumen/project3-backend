74b939dd5302cf670a439428177e881d63460438
michaloumen <michelle.lmendonca@gmail.com>
2021-06-10 22:16:48 -0300

    iniciando a conexao com banco de dados

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 import express from 'express';
 import data from './data';
+import dotenv from 'dotenv';
+import config from './config.js';
+import mongoose from 'mongoose';
+import userRoute from '../routes/userRoute';
 const cors = require('cors');
+
+dotenv.config();
+
+const mongodbUrl = config.MONGODB_URL;
+mongoose.connect(mongodbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}).catch(error => console.log(error.reason));
 
 const app = express();
 
 app.use(cors({
     origin: 'http://localhost:3000', //habilitando somente o front a consumir nossa API
 }));//habilitando nossa API para receber requisições de qualquer origem
+
+app.use("/api/users", userRoute);
 
 app.get("/api/products/:id", (req, res) => {
     const productId = req.params.id;
