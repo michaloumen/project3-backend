1713fd3b3fbdb5a1930ec7cc7535bcdc30d72eb6
michaloumen <michelle.lmendonca@gmail.com>
2021-06-12 13:19:53 -0300

    auth route - cadastro

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -9,7 +9,6 @@ router.post('/signin', async (req, res) => {
         email: req.body.email,
         password: req.body.password
     });
-    console.log(req.body.email, req.body.password)
     if (signinUser) {
         res.send({
             _id: signinUser.id,
@@ -19,9 +18,30 @@ router.post('/signin', async (req, res) => {
             token: getToken(signinUser)
         })
     } else {
-        res.status(401).send({ msg: 'Invalid Email or Password.' });
+        res.status(401).send({ msg: 'Email ou senha inválido' });
     }
 })
+
+router.post('/register', async (req, res) => {
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+    });
+    const newUser = await user.save();
+    console.log(req.body.email, req.body.password)
+    if (newUser) {
+        res.send({
+            _id: newUser.id,
+            name: newUser.name,
+            email: newUser.email,
+            isAdmin: newUser.isAdmin,
+            token: getToken(newUser),
+        });
+    } else {
+        res.status(401).send({ message: 'Invalid User Data.' });
+    }
+});
 
 router.get('/createadmin', async (req, res) => {
     try {
