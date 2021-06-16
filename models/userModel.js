b9dafd22d417719da5a28f7d411535710f47c168
michaloumen <michelle.lmendonca@gmail.com>
2021-06-16 19:52:51 -0300

    adicionando timestamps

--- models/userModel.js
+++ models/userModel.js
@@ -2,10 +2,12 @@ import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    email: { type: String, required: true, unique: true, dropDups: true },
+    email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     isAdmin: { type: Boolean, required: true, default: false }
-})
+}, {
+    timestamps: true
+});
 
 const userModel = mongoose.model("User", userSchema);
 
--- util.js
+++ util.js
@@ -12,30 +12,28 @@ export const getToken = (user) => {
     })
 }
 
-export const isAuth = (req, res, next) => {
-    const authorization = req.headers.authorization;
-    if (authorization) {
-        const token = authorization.slice(7, authorization.length); /* Barear XXXXXXx */
-        jwt.verify(token, process.env.JWT_SECRET, (err, decode) => {
-            if (err) {
-                return res.status(401).send({ msg: "Token Inválido" });
-            }
-            req.user = decode;
-            next();
-            return
-        });
-    } else {
-        return res.status(401).send({ msg: "Token não foi fornecido" })
-    }
-};
+// export const isAuth = (req, res, next) => {
+//     const authorization = req.headers.authorization;
+//     if (authorization) {
+//         const token = authorization.slice(7, authorization.length); /* Barear XXXXXXx */
+//         jwt.verify(token, process.env.JWT_SECRET, (err, decode) => {
+//             if (err) {
+//                 return res.status(401).send({ msg: "Token Inválido" });
+//             }
+//             req.user = decode;
+//             next();
+//             return
+//         });
+//     } else {
+//         return res.status(401).send({ msg: "Token não foi fornecido" })
+//     }
+// };
 
-export const isAdmin = (req, res, next) => {
-    if (req.user && req.user.isAdmin) {
-        return next();
-    }
-    return res.status(401).send({ msg: "Admin Token não é valido" })
-};
+// export const isAdmin = (req, res, next) => {
+//     if (req.user && req.user.isAdmin) {
+//         return next();
+//     }
+//     return res.status(401).send({ msg: "Admin Token não é valido" })
+// };
 
-/* isAdmin e isAuth não estão funcionando :( */
-
-
+/* isAdmin e isAuth não estão funcionando :() */
