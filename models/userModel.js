18f1eb97fc36831fefc60929474937ed2fd3635f
michaloumen <michelle.lmendonca@gmail.com>
2021-06-19 14:26:29 -0300

    consertando os problemas de fluxo de pagamento

--- models/userModel.js
+++ models/userModel.js
@@ -4,7 +4,10 @@ const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    isAdmin: { type: Boolean, required: true, default: false }
+    isAdmin: { type: Boolean, required: true, default: false },
+    address: { type: String, default: '' },
+    city: { type: String, default: '' },
+    postalCode: { type: String, default: '' }
 }, {
     timestamps: true
 });
